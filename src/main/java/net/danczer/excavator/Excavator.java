fa09477bde29662943bfe27e17ee0ae071ca47e7
László Tánczos <danczer1@gmail.com>
2022-11-27 15:56:47 +0100

    Clear Forge and init Fabric

---
+++ src/main/java/net/danczer/excavator/Excavator.java
@@ -0,0 +1,10 @@
+package net.danczer.excavator;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Excavator implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/danczer/excavator/client/ExcavatorClient.java
@@ -0,0 +1,13 @@
+package net.danczer.excavator.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ExcavatorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
