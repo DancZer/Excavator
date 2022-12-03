2dede4b59342e4b660f82f6c5c1b86aec3695335
László Tánczos <danczer1@gmail.com>
2022-12-03 09:47:51 +0100

    Remove hopper functionality from the excavator

--- src/main/java/net/danczer/excavator/ExcavatorMinecartEntity.java
+++ src/main/java/net/danczer/excavator/ExcavatorMinecartEntity.java
@@ -3,45 +3,34 @@ package net.danczer.excavator;
 import com.mojang.logging.LogUtils;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.Hopper;
-import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.entity.vehicle.StorageMinecartEntity;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.predicate.entity.EntityPredicates;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
 
-import java.util.List;
-
-public class ExcavatorMinecartEntity extends StorageMinecartEntity implements Hopper {
+public class ExcavatorMinecartEntity extends StorageMinecartEntity {
     private static final Logger LOGGER = LogUtils.getLogger();
     private static final TrackedData<Integer> MINING_STATUS;
 
     static {
         MINING_STATUS = DataTracker.registerData(ExcavatorMinecartEntity.class, TrackedDataHandlerRegistry.INTEGER);
     }
-    private static final float CollectBlockWithHardness = 3f;
     private static final float MinecartPushForce = 0.005f;
 
     private final ExcavationLogic excavationLogic = new ExcavationLogic(this, this);
     private boolean enabled = true;
     private int transferTicker = -1;
-    private final BlockPos lastPosition = BlockPos.ORIGIN;
 
     public ExcavatorMinecartEntity(EntityType<? extends StorageMinecartEntity> entityType, World world) {
         super(entityType, world);
@@ -101,18 +90,6 @@ public class ExcavatorMinecartEntity extends StorageMinecartEntity implements Ho
         return this.world;
     }
 
-    public double getHopperX() {
-        return this.getX();
-    }
-
-    public double getHopperY() {
-        return this.getY() + 0.5D;
-    }
-
-    public double getHopperZ() {
-        return this.getZ();
-    }
-
     public void onActivatorRail(int x, int y, int z, boolean powered) {
         boolean bl = !powered;
         if (bl != this.isEnabled()) {
@@ -122,7 +99,6 @@ public class ExcavatorMinecartEntity extends StorageMinecartEntity implements Ho
 
     public void tick() {
         excavatorTick();
-        hopperTick();
     }
 
     private void excavatorTick() {
@@ -188,7 +164,7 @@ public class ExcavatorMinecartEntity extends StorageMinecartEntity implements Ho
         }
 
         if (particleType != null) {
-            this.world.addParticle(particleType, this.getHopperX(), this.getHopperY() + 0.8D, this.getHopperZ(), 0.0D, 0.0D, 0.0D);
+            this.world.addParticle(particleType, this.getX(), this.getY() + 0.8D, this.getZ(), 0.0D, 0.0D, 0.0D);
         }
     }
 
@@ -198,61 +174,6 @@ public class ExcavatorMinecartEntity extends StorageMinecartEntity implements Ho
 
     private void setMiningStatus(ExcavationLogic.MiningStatus miningStatus) {
         this.dataTracker.set(MINING_STATUS, miningStatus.Value);
-    }
-
-    private void hopperTick() {
-        if (!this.world.isClient && this.isAlive() && this.isEnabled()) {
-            BlockPos blockpos = this.getBlockPos();
-            if (blockpos.equals(this.lastPosition)) {
-                --this.transferTicker;
-            } else {
-                this.setTransferTicker(0);
-            }
-
-            if (!this.canTransfer()) {
-                this.setTransferTicker(0);
-                if (this.captureDroppedItems()) {
-                    this.setTransferTicker(4);
-                    this.markDirty();
-                }
-            }
-        }
-    }
-
-    public void setTransferTicker(int transferTickerIn) {
-        this.transferTicker = transferTickerIn;
-    }
-
-    /**
-     * Returns whether the hopper cart can currently transfer an item.
-     */
-    public boolean canTransfer() {
-        return this.transferTicker > 0;
-    }
-
-    public boolean captureDroppedItems() {
-        List<ItemEntity> list = this.world.getEntitiesByClass(ItemEntity.class, this.getBoundingBox().expand(0.25, 0.0, 0.25), EntityPredicates.VALID_ENTITY);
-
-        for (ItemEntity itemEntity : list) {
-            Item item = itemEntity.getStack().getItem();
-
-            //collect only usefull blocks
-            if (item instanceof BlockItem blockItem) {
-                if (shouldCollectItem(blockItem)) {
-                    HopperBlockEntity.extract(this, itemEntity);
-                }
-            } else {
-                HopperBlockEntity.extract(this, itemEntity);
-            }
-        }
-
-        return false;
-    }
-
-    private boolean shouldCollectItem(BlockItem blockItem){
-        BlockState blockState = blockItem.getBlock().getDefaultState();
-
-        return blockState.isToolRequired() && blockState.getHardness(world, getBlockPos()) >= CollectBlockWithHardness;
     }
 
     @Override
